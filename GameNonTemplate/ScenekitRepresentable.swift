c51d5e380601b944bfb70fd547233178fe4c651d
Markus Kruusmägi <markus.kruusmagi@isotop.se>
2020-05-26 11:56:11 +0200

    Position and size of weights

--- GameNonTemplate/ScenekitRepresentable.swift
+++ GameNonTemplate/ScenekitRepresentable.swift
@@ -46,6 +46,7 @@ struct SceneKitView: UIViewRepresentable {
     class Coordinator: NSObject, SCNSceneRendererDelegate {
         var parent: SceneKitView
         var draggingNode: SCNNode?
+        var mass: CGFloat = 0
         var panStartZ: CGFloat?
         var lastPanLocation: SCNVector3?
 
@@ -57,6 +58,12 @@ struct SceneKitView: UIViewRepresentable {
         func renderer(_ renderer: SCNSceneRenderer, didSimulatePhysicsAtTime time: TimeInterval) {
             let node = parent.scene.rootNode.childNode(withName: "weightScale", recursively: true)!
             let verticalRod = parent.scene.rootNode.childNode(withName: "weightScaleVertical", recursively: true)!
+
+            let disk = parent.scene.rootNode.childNode(withName: "disk", recursively: true)!
+
+            print(disk.eulerAngles)
+//            node.presentation.eulerAngles.x = 0
+//            node.presentation.eulerAngles.z = 0
 
             if abs(node.presentation.eulerAngles.x) < 0.1 {
                 verticalRod.geometry?.firstMaterial?.diffuse.contents = UIColor.green
@@ -78,9 +85,11 @@ struct SceneKitView: UIViewRepresentable {
                 draggingNode = hitNodeResult.node
                 draggingNode?.geometry?.firstMaterial?.diffuse.contents = UIColor.green
                 draggingNode?.physicsBody?.allowsResting = false
+                mass = draggingNode?.physicsBody?.mass ?? 0
+                print("Mass: \(draggingNode!.physicsBody!.mass)")
                 draggingNode?.physicsBody?.isAffectedByGravity = false
                 draggingNode?.physicsBody?.type = .kinematic
-                print("Mass: \(draggingNode!.physicsBody!.mass)")
+
 
             case .changed:
                 guard let panStartZ = panStartZ else { return }
@@ -96,6 +105,7 @@ struct SceneKitView: UIViewRepresentable {
                 draggingNode?.geometry?.firstMaterial?.diffuse.contents = UIColor.red
                 draggingNode?.physicsBody?.type = .dynamic
                 draggingNode?.physicsBody?.isAffectedByGravity = true
+                draggingNode?.physicsBody?.mass = mass
                 draggingNode = nil
 
             default:
